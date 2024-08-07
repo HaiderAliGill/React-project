c46be7c53a9e5fc6f40e560f0e8544f53f751a9e
HaiderAliGill <haidergill09876@gmail.com>
2024-08-07 12:52:28 -0700

    react router add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./Component/Home/Home";
+import About from "./Component/About/About";
 
 function App() {
-
   return <>
-  dd</>;
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}  />
+          <Route path="/a" element={<About />}  />
+        </Routes>
+      </BrowserRouter>
+  </>;
 }
 
 export default App;
